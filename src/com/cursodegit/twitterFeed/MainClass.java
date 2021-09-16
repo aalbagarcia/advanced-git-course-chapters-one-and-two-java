5e99174ec6cca0fd15f1bd88f7ca5d41ac744602
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-09-16 18:20:48 +0200

    Modifica el saludo
    Una vez creado el commit inicial, modificamos nuestro código para
    cambiar el saludo que creamos inicialmente. En lugar de "Hello, World!"
    utilizaremos "¡Hola, caracola!". Vamos a hacer un par de commits antes
    de empezar a utilizar ramas. Este es el primero de ellos.
    
    After the first commit is created, we modify our code and replace the
    greeting. Instead of "Hello, World!" our program will say "¡Hola,
    caracola!". We will make a couple of commits before start using
    branches. This is the first of those commits.

--- src/com/cursodegit/twitterFeed/MainClass.java
+++ src/com/cursodegit/twitterFeed/MainClass.java
@@ -3,8 +3,7 @@ package com.cursodegit.twitterFeed;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		System.out.println("¡Hola, caracola!");
 	}
 
 }

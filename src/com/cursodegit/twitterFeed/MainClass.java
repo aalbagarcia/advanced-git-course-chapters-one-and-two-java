d9378543a19fa9bb8ba978ed72ce5b573c5d1b41
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-09-17 16:26:28 +0200

    [mostrar_tweets] Obtiene el token de autenticación.
    El siguiente paso en el desarrollo de la aplicación consiste en añadir
    una serie de funciones con las que nos "comunicaremos" con el API de
    Twitter.
    
    En este primer commit de la serie, añadimos la función getAccessToken,
    que nos devolverá un objeto de tipo Map que contendrá el token. En
    futuros commits, usaremos esta función para autenticarnos con el API
    de Twitter.
    
    ---------
    
    In the next step in the development of the application, we will add several
    functions to "talk" to the Twitter API.
    
    In this commit, the first in a series of four, we add the function
    getAccessToken. This function returns a Map object containing the auth
    token provided by Twitter.  In the next commits, we will use this function
    to authenticate and authorize the requests we send to the Twitter API.

--- src/com/cursodegit/twitterFeed/MainClass.java
+++ src/com/cursodegit/twitterFeed/MainClass.java
@@ -1,11 +1,51 @@
 package com.cursodegit.twitterFeed;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Base64;
 
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println("¡Hola, caracola!");
+		
+		try {
+			String accessToken = getAccessToken();
+			
+	        System.out.println(accessToken);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+	}
+	
+	public static String getAccessToken() throws IOException, InterruptedException {
+		
+        String oAuthConsumerKey = "yv4UDBXKGzuRf58FDzq6O8YVd";
+        String oAuthConsumerSecret = "pyWrYPTBHLleabhJ0diG8UIJNlpJDDxz56taz80kVvwvfRYlw8";
+        String aux = oAuthConsumerKey + ":" + oAuthConsumerSecret;
+		String consumerInfo = Base64.getEncoder().encodeToString(aux.getBytes());;
+		
+		System.out.println(aux);
+		System.out.println(consumerInfo);
+		
+		HttpClient client = HttpClient.newHttpClient();
+
+		HttpRequest request = HttpRequest.newBuilder(
+		       URI.create("https://api.twitter.com/oauth2/token"))
+		   .header("Authorization", "Basic " +  consumerInfo)
+		   .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+		   .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
+		   .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.statusCode());
+
+        return response.body();
 	}
 
 }

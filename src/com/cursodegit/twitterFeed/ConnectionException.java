8d586ae03d5f85111b9fd83d4cf4c61782b3ba97
Alfonso Alba García <alfonso@alfonsoalba.com>
2021-09-25 17:32:40 +0200

    [refactoring] Extrae la clase com.cursodegit.twitterFeed.Connection
    En esta sección del curso, vamos a realizar una integración con
    conflictos para aprender como resolverlos.
    
    Para ello, crearemos la rama refactoring en la que trabajaremos en
    local. En esta rama vamos a hacer dos refactorizaciones del código que
    nos vendrán bien más adelante cuando queramos crear los tests y
    automatizarlos.
    
    En este primer commit extraemos la clase Connection. Mientras tanto,
    nuestros compañeros seguirán trabajando la rama master subirán commits
    al servidor.
    
    ------
    
    
    In this section, we are going to merge two branches with conflicts.
    
    We will create a local branch called refactoring. In this branch we are
    going to refactor our code and prepare it to write some tests. Later, we
    will use our CI/CD system to run those tests automatically.
    
    In this first commit, we will extract the class Connection. At the same
    time, our colleagues will continue to work in the master branch,
    committing and pushing their changes.

---
+++ src/com/cursodegit/twitterFeed/ConnectionException.java
@@ -0,0 +1,14 @@
+package com.cursodegit.twitterFeed;
+
+public class ConnectionException extends Exception {
+
+	public ConnectionException(String message) {
+		super(message);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/com/cursodegit/twitterFeed/MainClass.java
+++ src/com/cursodegit/twitterFeed/MainClass.java
@@ -1,13 +1,9 @@
 package com.cursodegit.twitterFeed;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.Base64;
 import java.util.Map;
 import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 
 public class MainClass {
@@ -16,11 +12,13 @@ public class MainClass {
 	private static final String ANSI_RED = "\033[31;1m";
 	private static final String ANSI_GREEN = "\033[32;1m";
 	
-	private static Map<String, String> accessTokenMap;
 	private static String twitterHandle = "";
 	
 	// Comentarios añadidos por nuestro compañeros (simulado como commit en Bitbucket/Github/Gitlab)
 	public static void main(String[] args) throws Exception {
+	    String oAuthConsumerKey = "yv4UDBXKGzuRf58FDzq6O8YVd";
+	    String oAuthConsumerSecret = "pyWrYPTBHLleabhJ0diG8UIJNlpJDDxz56taz80kVvwvfRYlw8";
+	    
 		if ( args.length == 0 ) {
 			System.out.println(ANSI_RED+"Debes introducir el usuario de twitter."+ANSI_RESET);
 			System.out.println(Usage());
@@ -29,8 +27,9 @@ public class MainClass {
 		twitterHandle = args[0];
 		
 		try {
-			accessTokenMap = getAccessToken();
-			List<Map<String, Object>> tweets = getTweets(twitterHandle, 10);
+			Connection conn = new Connection(oAuthConsumerKey, oAuthConsumerSecret);
+			
+			List<Map<String, Object>> tweets = getTweets(conn, twitterHandle, 10);
 			for (int i=0; i < tweets.size(); i++) {
 				String line = String.format("%s\t%s", tweets.get(i).get("created_at"), tweets.get(i).get("text"));
 				System.out.println(line); 
@@ -41,55 +40,15 @@ public class MainClass {
 		}
 	}
 	
-	// Comentarios añadidos por nuestro compañeros (simulado como commit en Bitbucket/Github/Gitlab)
-	public static Map<String, String> getAccessToken() throws IOException, InterruptedException {
-		
-        String oAuthConsumerKey = "yv4UDBXKGzuRf58FDzq6O8YVd";
-        String oAuthConsumerSecret = "pyWrYPTBHLleabhJ0diG8UIJNlpJDDxz56taz80kVvwvfRYlw8";
-        String aux = oAuthConsumerKey + ":" + oAuthConsumerSecret;
-		String consumerInfo = Base64.getEncoder().encodeToString(aux.getBytes());;
-		
-		System.out.println(aux);
-		System.out.println(consumerInfo);
-		
-		// Get the token
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(
-		       URI.create("https://api.twitter.com/oauth2/token"))
-		   .header("Authorization", "Basic " +  consumerInfo)
-		   .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-		   .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
-		   .build();
-
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-
-        String body = response.body();
-	    Map<String, String> map = new ObjectMapper().readValue(body, Map.class);
-
-		
-        return map;
-	}
 
 	// Comentarios añadidos por nuestro compañeros (simulado como commit en Bitbucket/Github/Gitlab)
-	public static List<Map<String, Object>> getTweets(String username, int count) throws IOException, InterruptedException {
+	public static List<Map<String, Object>> getTweets(Connection conn, String username, int count) throws ConnectionException, JsonMappingException, JsonProcessingException {
 		String uri = String.format("https://api.twitter.com/1.1/statuses/user_timeline.json?count=%d&screen_name=%s&trim_user=1&exclude_replies=1", count, username );
-		// Get the token
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(
-		       URI.create(uri))
-		   .header("Authorization", "Bearer " +  accessTokenMap.get("access_token"))
-		   .GET()
-		   .build();
 
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-
-		String body = response.body();
+		String body = conn.get(uri);
 		System.out.println(body);
 	    List<Map<String, Object>> map = new ObjectMapper().readValue(body, List.class);
-
-		
+	
         return map;
 	}
 	
